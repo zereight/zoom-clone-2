e6d4153277deed5108c77a7c405ad562b7955fb5
zereight <zereight@zereightui-MacBookPro.local>
2020-08-10 20:50:52 +0900

    init

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+server.listen(3000, () => {
+  console.log("Server started");
+});
