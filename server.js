3810cedf601b945ff8e2bd0eb610b0c13e309bb2
zereight <zereight@zereightui-MacBookPro.local>
2020-08-11 01:13:43 +0900

    socket.io applied

--- server.js
+++ server.js
@@ -4,6 +4,31 @@ const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
+const { v4: uuidV4 } = require("uuid");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidV4()}`);
+});
+
+app.get("/:room", (req, res) => {
+  const { room } = req.params;
+  res.render("room", { roomId: room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+
+    socket.on("disconnect", () => {
+      socket.to(roomId).broadcast.emit("user-disconnected", userId);
+    });
+  });
+});
+
 server.listen(3000, () => {
   console.log("Server started");
 });
